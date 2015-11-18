4823352d2a5bc81c45058beddb9879d3c5beaf4a
Arief Darmawan <ariefda@hotmail.com>
2015-11-18 21:46:50 +0700

    add job to monitor

--- coderun.go
+++ coderun.go
@@ -20,12 +20,33 @@ type CodeModule struct {
 	Path       string
 }
 
-type Code struct {
+type CodeBase struct {
 	Id          string
-	Title       string
-	CodeTypeId  string
 	CodeStorage CodeStorageEnum
 	Txt         string
+	Title       string
+}
+
+type CodeOp struct {
+	Op      string
+	Command string
+	Params  []interface{}
+}
+
+type Code struct {
+	Id         string
+	Title      string
+	CodeTypeId string
+	Modules    []*CodeModule
+	CodeBases  []*CodeBase
+
+	// ops will be used for compile, build and exec
+	// if nil for each of those, it will use standard tool from CodeType
+	Ops map[string]*CodeOp
+}
+
+func (c *Code) CodeType() *CodeType {
+	return nil
 }
 
 func (c *Code) Compile() error {
---
+++ job.go
@@ -0,0 +1,19 @@
+package codebase
+
+type Job struct {
+	Id     int
+	CodeId string
+	Status string
+}
+
+func (j *Job) Init() {
+
+}
+
+func (j *Job) Start() {
+
+}
+
+func (j *Job) Stop() {
+
+}
